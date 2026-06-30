df514da7426623f76a231982c0b974b920e2a848
kimootaku <501jfw.s.witches@gmail.com>
2026-06-30 19:20:05 +0900

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/chunithm-calc/',
   plugins: [
     react(),
     tailwindcss(),
